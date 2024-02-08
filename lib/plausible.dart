c585f9b68bccebd7d7a87288828b4704ddec73b5
ethicnology <ethicnology@pm.me>
2024-02-08 17:44:16 -0500

    chore: remove useless elements

--- lib/plausible.dart
+++ lib/plausible.dart
@@ -1,8 +1,6 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library;
+library plausible;
 
 export 'src/plausible_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
